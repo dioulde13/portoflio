de08fbbee60ca0c612eafa96902a8b30dce577c9
ecash <dioulde.diallo@ecash-guinee.com>
2025-11-07 16:00:17 +0000

    arrang texte

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 export default function Home() {
   return (
-    <section className="w-11/12 md:w-4/5 lg:w-[97%] mx-auto text-center py-4 mb-4 flex flex-col items-center bg-gray-600 text-white">
+    <section className="w-11/12 md:w-4/5 lg:w-[97%] mx-auto py-4 mb-4 flex flex-col items-center bg-gray-600 text-white">
       <div className="mb-6 overflow-hidden rounded-t-full rounded-b-2xl w-48 h-64 shadow-xl border-4 border-gray-800">
         <Image
           src="/images/dioulde.jpg"
@@ -35,9 +35,12 @@ export default function Home() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.2 }}
-        className="mt-4 text-base text-white-300 max-w-2xl mx-auto"
+        className="mt-4 text-base text-white-300 max-w-2xl"
       >
-         Titulaire d’une licence professionnelle en Génie Informatique/Génie Logiciel de l’Université Nongo Conakry. Développeur Web et mobile hautement qualifié, je cumule plus de 4 ans d’expérience dans les technologies telles que HTML, CSS, Tailwind et JavaScript. Ma maîtrise de frameworks comme Angular, Flutter et Node.js, ainsi que de la bibliothèque React.js, me permet de concevoir et d’optimiser des solutions logicielles performantes et ergonomiques.
+         Titulaire d’une licence professionnelle en Génie Informatique/Génie Logiciel de l’Université Nongo Conakry. 
+         Développeur Web et mobile hautement qualifié, je cumule plus de 4 ans d’expérience dans les technologies telles que HTML, CSS, Tailwind et JavaScript.
+          Ma maîtrise de frameworks comme Angular, Flutter et Node.js, ainsi que de la bibliothèque React.js,
+           me permet de concevoir et d’optimiser des solutions logicielles performantes et ergonomiques.
       </motion.p>
 
       {/* Bouton vers projets */}
